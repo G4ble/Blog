53c4ff90ec7585372041d1a8e88a99e68dca2ea7
Udo Sperling <usperling35@gmail.com>
2025-06-06 20:26:07 +0200

    tried to fix routing 3

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ import (
 
 func main() {
 	http.HandleFunc("/static/", routing.StaticHandler)
-	http.HandleFunc("/game", routing.GameHandler)
+	http.HandleFunc("/game/", routing.GameHandler)
 	http.HandleFunc("/*", routing.IndexHandler)
 
 	GracefulListenAndServe(":900", nil)
